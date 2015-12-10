ca2f1099ca32b7f317d0683044d9586d6578f589
Hernan Y.Ke <yuriqiao@gmail.com>
2015-12-10 14:22:57 -0500

    cart session add and save func

---
+++ cart/cart.py
@@ -0,0 +1,26 @@
+__author__ = 'Hernan Y.Ke'
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+
+class Cart(object):
+
+    def __init__(self,request):
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID]={}
+        self.cart = cart
+    def add(self, product, quantity=1,update_quantity=False):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id]={'quantity':0,'price':str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity']=quantity
+        else:
+            self.cart[product_id]['quantity']+=quantity
+        self.save()
+
+    def save(self):
+        self.session[settings.CART_SESSION_ID]==self.cart
+        self.session.modified = True
--- pkdjbyexampj3/settings.py
+++ pkdjbyexampj3/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,7 +102,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
+
+CART_SESSION_ID = 'cart'
