fb24a2724557ad19ccef13e30a614b932c32d55e
Hernan Y.Ke <yuriqiao@gmail.com>
2015-12-10 14:35:38 -0500

    cart remove func and __iter__

--- cart/cart.py
+++ cart/cart.py
@@ -24,3 +24,20 @@ class Cart(object):
     def save(self):
         self.session[settings.CART_SESSION_ID]==self.cart
         self.session.modified = True
+
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product.id]
+            self.save()
+
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product']=product
+        for item in self.cart.values():
+            item['price']=Decimal(item['price'])
+            item['total_price']=item['price']*item['quantity']
+            yield item
