ace30b9a67fced85ee43407b25e42d64315827d9
Hernan Y.Ke <yuriqiao@gmail.com>
2015-12-10 13:08:29 -0500

    basic model

---
+++ shop/migrations/0001_initial.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('name', models.CharField(db_index=True, max_length=200)),
+                ('slug', models.SlugField(unique=True, max_length=200)),
+            ],
+            options={
+                'ordering': ('name',),
+                'verbose_name_plural': 'categories',
+                'verbose_name': 'category',
+            },
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('name', models.CharField(db_index=True, max_length=200)),
+                ('slug', models.SlugField(max_length=200)),
+                ('image', models.ImageField(upload_to='products/%Y/%m/%d', blank=True)),
+                ('description', models.TextField(blank=True)),
+                ('price', models.DecimalField(max_digits=10, decimal_places=2)),
+                ('stock', models.PositiveIntegerField()),
+                ('available', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(to='shop.Category', related_name='products')),
+            ],
+            options={
+                'ordering': ('name',),
+            },
+        ),
+        migrations.AlterIndexTogether(
+            name='product',
+            index_together=set([('id', 'slug')]),
+        ),
+    ]
